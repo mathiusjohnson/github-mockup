199e6ae85441596db97296afc675ffdd0dd20d3e
Mathius Johnson <devachievem@gmail.com>
2021-01-13 15:39:06 -0800

    retrieving the rest of the api from github url

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from 'express';
 import yourJson from '../../data/repos.json';
+const request = require('request');
 
 export const repos = Router();
+
+const url = 'https://api.github.com/users/silverorange/repos';
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -10,6 +13,19 @@ repos.get('/', async (_: Request, res: Response) => {
   // console.log(yourJson);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
+  let data;
+  request({
+    url: url,
+    json: true
+  }, function (error: any, response: {statusCode: number;}, body: any) {
+      if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+        data = body;
+    }
+    console.log(data);
+
+  })
+
   for (const repository of yourJson) {
     if (repository.fork !== false) {
       res.json(repository);

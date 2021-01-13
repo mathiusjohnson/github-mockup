b4b7f29f1d4299b9c7981c20780de2f2f20e01ca
Mathius Johnson <devachievem@gmail.com>
2021-01-13 14:23:49 -0800

    json data looped through and JSON response sent to localhost with appropriate data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import yourJson from '../../data/repos.json';
 
 export const repos = Router();
 
@@ -6,7 +7,12 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
+  // console.log(yourJson);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  for (const repository of yourJson) {
+    if (repository.fork !== false) {
+      res.json(repository);
+    }
+  }
 });

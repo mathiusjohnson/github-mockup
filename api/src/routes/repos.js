babd5778c2c28e4639ae8f6f949641a2c26e5e98
Mathius Johnson <devachievem@gmail.com>
2021-01-13 20:42:40 -0800

    linted and prettier api

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,58 +1,35 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import yourJson from '../../data/repos.json';
 import https from 'https';
 
 export const repos = Router();
-
-const url = 'https://api.github.com/users/silverorange/repos';
 
 const username = 'silverorange';
 const options = {
   hostname: 'api.github.com',
   path: '/users/' + username + '/repos',
   method: 'GET',
-  headers: {'user-agent': 'node.js'}
-
+  headers: { 'user-agent': 'node' },
 };
 
-let body = "";
+let body = '';
 
-const request = https.request(options, function(response){
-  response.on("data", function(chunk){
-      body += chunk;
+const request = https.request(options, function (response) {
+  response.on('data', function (chunk) {
+    body += chunk;
   });
+});
+request.end();
 
-  response.on("end", function(){
-      // console.log("Body: ", body);
-      });
-  });
-
-  request.end();
-
-// console.log(typeof body);
-// body = body.filter(repo => repo.fork === true)
-
-
-// console.log(body);
 repos.get('/', async (req, res) => {
   res.header('Cache-Control', 'no-store');
-  let data = JSON.parse(body)
-  console.log(data.length);
-  data = data.filter(repo => repo.fork === true)
-  console.log(data.length);
-  // console.log(typeof JSON.parse(body));
-  // for (const repo of body) {
-  //   console.log(repo);
-  //   // if (repo.fork !== false) {
-  //   //   console.log("true");
-  //   // }
-  // }
-  // console.log(yourJson);
+  let data = JSON.parse(body);
+  data = data.filter((repo) => repo.fork === false);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   for (const repository of yourJson) {
-    if (repository.fork !== false) {
-      res.json(repository);
+    if (repository.fork === false) {
+      data.push(repository);
+      res.json(data);
     }
   }
 });

4c27741110d87cbc46213cfb183fe99d1f0087c8
Mathius Johnson <devachievem@gmail.com>
2021-01-19 22:12:44 -0800

    languagesAPI.ts TS error resolved

--- web/src/features/languages/languagesAPI.ts
+++ web/src/features/languages/languagesAPI.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+// export interface Language {
+//   [key: string]: unknown,
+//   id: number,
+//   language: string,
+//   status: string,
+//   error: string | undefined | undefined
+// }
+
+// interface GetLanguagesResult {
+//   languages:  Language[];
+// }
+
 export interface Language {
   [key: string]: unknown,
   id: number,
@@ -8,17 +20,19 @@ export interface Language {
   error: string | undefined | undefined
 }
 
-interface GetLanguagesResult {
-  languages: Language[];
+export interface GetLanguagesResult {
+  languages: {
+      [key: string]: Language
+  };
 }
 
 export async function getLanguages(): Promise<GetLanguagesResult> {
   const url = 'http://localhost:4000/languages'
 
-  const languagesResponse = await axios.get<{data: Language[]}>(url);
-  console.log("data: ", languagesResponse.data);
-  return {
+  const languagesResponse = await axios.get<{data: Language}>(url);
 
+  console.log("data: ", typeof languagesResponse.data);
+  return {
     languages: languagesResponse.data,
   };
 }

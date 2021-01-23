dfda46ad286c2d7068b377e483a07309197c24d3
Mathius Johnson <devachievem@gmail.com>
2021-01-22 23:12:40 -0800

    remove routes/languages.ts, add languages route to repos file to DRY code

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -8,6 +8,10 @@ export const repos = Router();
 console.log(typeof getAPI);
 
 console.log("this repo file works");
+
+let repoData: any;
+
+
 repos.get('/', async (req, res) => {
 
   const octokit = new Octokit({
@@ -20,12 +24,39 @@ repos.get('/', async (req, res) => {
       type: "public",
     })
     .then(({ data }: {data:any}) => {
+
       data = data.filter((repo: {fork: boolean;}) => repo.fork === false)
+
       for (const repository of yourJson) {
           if (repository.fork === false) {
             data.push(repository);
+            repoData = data;
             res.json(data);
           }
         }
     });
+});
+
+export const languages = Router();
+
+languages.get('/', async (req, res) => {
+
+  let languages: any = {};
+  let id = 0;
+  for (const key in repoData) {
+    if (Object.hasOwnProperty.call(repoData, key)) {
+      const repo = repoData[key];
+      if (repo.fork !== false) {
+        continue;
+      }
+      if (repo.language === null) {
+        continue;
+      }
+      if (!languages[repo.language]) {
+        languages[repo.language] = {id, language: repo.language};
+      }
+    }
+    id++
+  }
+  res.json(languages)
 });
